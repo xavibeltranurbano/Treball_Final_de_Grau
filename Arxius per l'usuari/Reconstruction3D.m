de25a8e646d9430050223688ab75c7b0129c8525
Xavier Beltran Urbano <81012437+xavibeltranurbano@users.noreply.github.com>
2022-06-07 21:26:28 +0200

    Update Reconstruction3D.m

--- Arxius per l'usuari/Reconstruction3D.m
+++ Arxius per l'usuari/Reconstruction3D.m
@@ -1,29 +1,12 @@
 clc, close all, clear all;
-path="/Users/xavibeltranurbano/Desktop/UNIVERSITAT/TFG";
 
-skull=niftiread(path+'/PROVA/Brats18_TCIA02_374_1/SPM/c4Brats18_TCIA02_374_1_t1.nii');
-%skull=imclose(skull,strel('disk',5));
-grey_matter=niftiread(path+'/PROVA/Brats18_TCIA02_374_1/SPM/c1Brats18_TCIA02_374_1_t1.nii');
-white_matter=niftiread(path+'/PROVA/Brats18_TCIA02_374_1/SPM/c2Brats18_TCIA02_374_1_t1.nii');
-csf=niftiread(path+'/PROVA/Brats18_TCIA02_374_1/SPM/c3Brats18_TCIA02_374_1_t1.nii');
-lesio=niftiread(path+'/PROVA/Brats18_TCIA02_374_1/Mask_Brats18_TCIA02_374_1_kmeans.nii');
-info=niftiinfo(path+'/PROVA/Brats18_TCIA02_374_1/Mask_Brats18_TCIA02_374_1_kmeans.nii.nii');
-voxel_size=info.PixelDimensions
-% lesion=isosurface(im);
-% skull_2=isosurface(skull);
-% %%
-% patch(lesion,'FaceColor','r','EdgeColor','none');
-% patch(skull_2,'FaceColor','y','EdgeColor','none','FaceAlpha',0.5);
-% view(45,45); daspect(1./voxel_size); 
-% axis off;
-% camlight(-100,-100);  lighting phong;
+path="C:\Users\GESTIO\Desktop\";
+codi_pacient=98734;
+crani=niftiread(path+"Imatges Pacient\c4PACIENT_"+codi_pacient+"_t1.nii");
+lesio=niftiread(path+"Resultats Segmentació\Segmentacio.nii");
 
-% final=double(im)+imbinarize(skull);
-% figure, imagesc(final(:,:,100))
-%%
-
-%SKULL
-skull(skull>1)=1;
+%CRANI
+crani(crani>1)=1;
 
 %LESIO
 lesio(lesio>1)=1;
@@ -31,49 +14,25 @@ lesio_petita=imerode(lesio, strel('disk',5));
 lesio_petita2=~lesio_petita;
 lesio2=~lesio;
 
-%GREY_MATTER
-grey_matter(grey_matter>1)=1;
-grey_matter=imbinarize(grey_matter).*lesio2;
-grey_matter1=grey_matter(1:100,:,:);
-grey_matter2=grey_matter(101:end,:,:);
-
-%WHITE MATTER
-white_matter(white_matter>1)=1;
-white_matter=imbinarize(white_matter).*lesio2;
-white_matter1=white_matter(1:100,:,:);
-white_matter2=white_matter(101:end,:,:);
-
-%CSF
-csf(csf>1)=1;
-csf=imbinarize(csf).*lesio2;
-csf1=csf(1:100,:,:);
-csf2=csf(101:end,:,:);
-
-%SKULL+LESIO
-final_tot=imbinarize(skull).*lesio2;
+%CRANI+LESIO
+final_tot=imbinarize(crani).*lesio2;
 final_tot=final_tot+lesio;
 final_tot=final_tot.*lesio_petita2;
 
 
+%FEM TALL TRANSVERSAL
+final_tot1=final_tot(1:100,:,:);
+final_tot2=final_tot(101:end,:,:);
+
 %FEM TALL SAGITAL
-%final_tot1=final_tot(1:100,:,:);
-%final_tot2=final_tot(101:end,:,:);
-final_tot1=final_tot(:,:,1:81);
-final_tot2=final_tot(:,:,81:end);
+final_tot3=final_tot(:,:,1:81);
+final_tot4=final_tot(:,:,81:end);
+
 
 %% CREEM ARCHIU STL
 
-[Vertices, Triangle, Quads] = make_STL_of_Array('/Users/xavibeltranurbano/Desktop/UNIVERSITAT/TFG/PROVA/Brats18_TCIA02_374_1/MESHLAB/skull_tumor1.stl',final_tot1,1,1,1); 
-[Vertices, Triangle, Quads] = make_STL_of_Array('/Users/xavibeltranurbano/Desktop/UNIVERSITAT/TFG/PROVA/Brats18_TCIA02_374_1/MESHLAB/lesio.stl',lesio_petita,1,1,1); 
-[Vertices, Triangle, Quads] = make_STL_of_Array('/Users/xavibeltranurbano/Desktop/UNIVERSITAT/TFG/PROVA/Brats18_TCIA02_374_1/MESHLAB/skull_tumor2.stl',final_tot2,1,1,1); 
-[Vertices, Triangle, Quads] = make_STL_of_Array('/Users/xavibeltranurbano/Desktop/UNIVERSITAT/TFG/PROVA/Brats18_TCIA02_374_1/MESHLAB/skull_tumor_grey.stl',final_tot,1,1,1);
-% [Vertices, Triangle, Quads] = make_STL_of_Array('/Users/xavibeltranurbano/Desktop/UNIVERSITAT/TFG/PROVA/Brats18_TCIA02_374_1/MESHLAB/grey1.stl',grey_matter1,1,1,1);
-% [Vertices, Triangle, Quads] = make_STL_of_Array('/Users/xavibeltranurbano/Desktop/UNIVERSITAT/TFG/PROVA/Brats18_TCIA02_374_1/MESHLAB/white1.stl',white_matter1,1,1,1);
-% [Vertices, Triangle, Quads] = make_STL_of_Array('/Users/xavibeltranurbano/Desktop/UNIVERSITAT/TFG/PROVA/Brats18_TCIA02_374_1/MESHLAB/csf1.stl',csf1,1,1,1);
-% [Vertices, Triangle, Quads] = make_STL_of_Array('/Users/xavibeltranurbano/Desktop/UNIVERSITAT/TFG/PROVA/Brats18_TCIA02_374_1/MESHLAB/grey2.stl',grey_matter2,1,1,1);
-% [Vertices, Triangle, Quads] = make_STL_of_Array('/Users/xavibeltranurbano/Desktop/UNIVERSITAT/TFG/PROVA/Brats18_TCIA02_374_1/MESHLAB/white2.stl',white_matter2,1,1,1);
-% [Vertices, Triangle, Quads] = make_STL_of_Array('/Users/xavibeltranurbano/Desktop/UNIVERSITAT/TFG/PROVA/Brats18_TCIA02_374_1/MESHLAB/csf2.stl',csf2,1,1,1);
-% 
-% 
-% 
-% 
+[Vertices, Triangle, Quads] = make_STL_of_Array('C:\Users\GESTIO\Desktop\Resultats Reconstrucció\sagital1.stl',final_tot1,1,1,1); 
+[Vertices, Triangle, Quads] = make_STL_of_Array('C:\Users\GESTIO\Desktop\Resultats Reconstrucció\sagital2.stl',final_tot2,1,1,1); 
+[Vertices, Triangle, Quads] = make_STL_of_Array('C:\Users\GESTIO\Desktop\Resultats Reconstrucció\transversal1.stl',final_tot3,1,1,1); 
+[Vertices, Triangle, Quads] = make_STL_of_Array('C:\Users\GESTIO\Desktop\Resultats Reconstrucció\transversal2.stl',final_tot4,1,1,1);
+[Vertices, Triangle, Quads] = make_STL_of_Array('C:\Users\GESTIO\Desktop\Resultats Reconstrucció\tumor.stl',lesio_petita,1,1,1);
